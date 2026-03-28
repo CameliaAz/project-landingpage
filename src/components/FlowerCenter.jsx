d1d8660165a042c87af71094cb059dbb5e11b74d
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2026-03-29 01:03:07 +0700

    adjust responsive for bungarambat

--- src/components/FlowerCenter.jsx
+++ src/components/FlowerCenter.jsx
@@ -1,13 +1,13 @@
 export default function FlowerCenter() {
   return (
-    <div className="absolute top-10 md:top-20 left-0 w-full flex justify-center z-10 pointer-events-none">
+    <div className="absolute top-12 md:top-20 left-0 w-full flex justify-center z-10 pointer-events-none">
       <div className="relative w-full max-w-7xl h-0">
 
         {/* Rambat kiri */}
         <img
           src="/assets/bungarambat.png"
           alt="bungarambat kiri"
-          className="absolute -top-10 xs:-top-20 sm:-top-40 md:-top-80 right-1/2 
+          className="absolute -top-8 xs:-top-2 sm:-top-20 md:-top-80 right-1/2 
                      w-[160px] xs:w-[220px] sm:w-[380px] md:w-[670px] object-contain"
         />
 
@@ -15,7 +15,7 @@ export default function FlowerCenter() {
         <img
           src="/assets/bungarambat.png"
           alt="bungarambat kanan"
-          className="absolute -top-10 xs:-top-20 sm:-top-40 md:-top-80 left-1/2 
+          className="absolute -top-8 xs:-top-2 sm:-top-20 md:-top-80 left-1/2 
                      w-[160px] xs:w-[220px] sm:w-[380px] md:w-[670px] object-contain scale-x-[-1]"
         />
 
@@ -23,7 +23,7 @@ export default function FlowerCenter() {
         <img
           src="/assets/bunga1.png"
           alt="center flower"
-          className="absolute top-2 xs:top-4 md:top-10 left-1/2 -translate-x-1/2 
+          className="absolute top-10 xs:top-6 md:top-10 left-1/2 -translate-x-1/2 
                      w-14 xs:w-16 md:w-28 lg:w-34 z-20"
         />
       </div>
